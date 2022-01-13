d4aab2f0b2543a22292ffb724f220efec7b35abe
xepaul <xepaul@users.noreply.github.com>
2022-01-13 12:48:16 +0000

    start using MonadState

--- src/Rosalind/Problems/Tran.hs
+++ src/Rosalind/Problems/Tran.hs
@@ -1,43 +1,62 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+
 module Rosalind.Problems.Tran where
 
-import Control.Lens 
-import Rosalind.DnaBase  (DnaBase(..))
+import Control.Lens
+import Control.Monad.State (MonadState (), execState)
+import Data.Monoid.Generic
+  ( GenericMonoid (..),
+    GenericSemigroup (..),
+  )
+import Data.Semigroup (Sum (..))
+import GHC.Generics (Generic)
+import Rosalind.DnaBase (DnaBase (..))
 
-data Stats a b = Stats   { _transitions ::Double, 
-                          _transversions ::Double}
+data Stats = Stats
+  { _transitions :: Sum Double,
+    _transversions :: Sum Double
+  }
+  deriving (Generic)
+  deriving (Monoid) via GenericMonoid Stats
+  deriving (Semigroup) via GenericSemigroup Stats
 
 makeLenses ''Stats
 
 trans :: [DnaBase] -> [DnaBase] -> Double
-trans s1 s2 = let s  = countTransversion s1 s2
-               in  ( s ^. transitions)
-                   / ( s ^. transversions)
-    where
-        countTransversion :: [DnaBase] -> [DnaBase ] -> Stats Int Int
-        countTransversion d1 d2 = foldl updateStats (Stats 0 0) $ zip  d1 d2
-
-        updateStats :: Stats Int Int -> (DnaBase, DnaBase)  -> Stats Int Int
-        updateStats s (a, b) = 
-            case (a,b) of
-            (A, A) ->  s
-            (C, C) ->  s
-            (G, G) ->  s
-            (T, T) ->  s
-            (A, G) -> incTransitions 
-            (G, A) -> incTransitions 
-            (C, T) -> incTransitions 
-            (T, C) -> incTransitions 
-            (A, C) -> incTransversions
-            (A, T) -> incTransversions
-            (G, C) -> incTransversions
-            (G, T) -> incTransversions
-            (C, A) -> incTransversions
-            (C, G) -> incTransversions
-            (T, A) -> incTransversions
-            (T, G) -> incTransversions
-            where 
-              incTransitions = s & transitions +~ 1
-              incTransversions = s & transversions +~ 1
+trans s1 s2 =
+  let s = countTransversion s1 s2
+   in getSum (s ^. transitions) / getSum (s ^. transversions)
+  where
+    countTransversion :: [DnaBase] -> [DnaBase] -> Stats
+    countTransversion d1 d2 = execState (getStats d1 d2) mempty
+    getStats :: (MonadState Stats m) => [DnaBase] -> [DnaBase] -> m ()
+    getStats d1 d2 =
+      mapM_ \case
+        (A, A) -> noChange
+        (C, C) -> noChange
+        (G, G) -> noChange
+        (T, T) -> noChange
+        (A, G) -> incTransitions
+        (G, A) -> incTransitions
+        (C, T) -> incTransitions
+        (T, C) -> incTransitions
+        (A, C) -> incTransversions
+        (A, T) -> incTransversions
+        (G, C) -> incTransversions
+        (G, T) -> incTransversions
+        (C, A) -> incTransversions
+        (C, G) -> incTransversions
+        (T, A) -> incTransversions
+        (T, G) -> incTransversions
+        $ zip d1 d2
+      where
+        incTransitions = transitions += 1
+        incTransversions = transversions += 1
+        noChange = transitions += 0
