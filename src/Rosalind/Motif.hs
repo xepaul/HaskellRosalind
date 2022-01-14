60a479eb24d0e7fbc6cecb7be984e29d3ea215b3
xepaul <xepaul@users.noreply.github.com>
2022-01-14 08:39:23 +0000

    add motif QuasiQuoter

--- src/Rosalind/Motif.hs
+++ src/Rosalind/Motif.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveLift #-}
 module Rosalind.Motif where
 
 import Text.Parsec.Prim (parse)
@@ -10,15 +13,17 @@ import Text.ParserCombinators.Parsec
   )
 import Text.Printf
 import Data.Text qualified as T
-
 import Data.Set qualified as Set
 import Data.Either.Combinators
 import Rosalind.ProteinWithStop
 import Data.List.Extra
-data Motif a = MotifValue a
-               |  MotifAnyExcept [a]
-               |  MotifOption [a]
-        deriving (Show,Eq)
+import Language.Haskell.TH
+import Language.Haskell.TH.Quote 
+import Language.Haskell.TH.Syntax ( Lift )
+data Motif a     = MotifValue a
+                        |  MotifAnyExcept [a]
+                        |  MotifOption [a]
+                  deriving (Show,Eq,Lift)
 
 
 
@@ -45,7 +50,6 @@ motifValueParser p = MotifValue <$> p
 motifParser :: Parser a -> Parser [Motif a]
 motifParser p = many1 (choice [motifValueParser p,motifAnyExceptParser p,motifOptionParser p])
 
---parseProteinMotif :: String ->  Either String [Motif ProteinWithStop]
 parseProteinMotif :: String -> Either String [Motif ProteinWithStop]
 parseProteinMotif  = mapLeft show <$> parse (motifParser motifProteinWithStopParser) ""
 
@@ -61,5 +65,21 @@ showMotif = foldl (\s m -> case m of
                                                   )
                   []
 
+makeProteinWithStopMotif :: String -> Q Exp
+makeProteinWithStopMotif name = case parseProteinMotif name of
+  Left e -> fail $ "Invalid Motif should be one of " <> concatMap show  (enumerate @ProteinWithStop) <> " " <> show e
+  Right v -> [|v|]
+
+proteinWithStopMotifString :: QuasiQuoter
+proteinWithStopMotifString =
+  QuasiQuoter
+    { quoteExp = makeProteinWithStopMotif,
+      quotePat = error "quote: Invalid application in pattern context.",
+      quoteType = error "quote: Invalid application in pattern context.",
+      quoteDec = error "quote: Invalid application in pattern context."
+    }
+
 -- >>> parseProteinMotif "N{P}[ST]{P*}"
 -- Right [MotifValue N,MotifAnyExcept [P],MotifOption [S,T],MotifAnyExcept [P,Stop]]
+-- >>> [proteinWithStopMotifString|N{P}[ST]{P*}|]
+-- [MotifValue N,MotifAnyExcept [P],MotifOption [S,T],MotifAnyExcept [P,Stop]]
