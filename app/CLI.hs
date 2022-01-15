85a186f4bfaea4e1b9e8aab4cefeb1cb9cdb5d5b
xepaul <xepaul@users.noreply.github.com>
2022-01-15 10:44:43 +0000

    add ouput file option to cli

--- app/CLI.hs
+++ app/CLI.hs
@@ -37,7 +37,8 @@ getCommandName = T.unpack . T.toLower . T.pack . show
 
 data Options = Options
   { optCommands :: ProblemCommands,
-    optDataSetOption :: DatasetOption
+    optDataSetOption :: DatasetOption,
+    optOutOption :: String 
   }
 
 data DatasetOption = RealDataset | ExampleDataset deriving (Show)
@@ -66,9 +67,15 @@ commands = subparser (foldMap mkCommand (enumerate @ProblemCommands)
        in command
             name
             ( info
-                (RunProblem  <$>  (Options c <$> useExampleDataset))
+                (RunProblem  <$>  (Options c <$> useExampleDataset <*> outOptionParser ))
                 (progDesc $ "Execute problem " <> filter isLetter name)
             )
+    outOptionParser = strOption
+                    ( long "output"
+                    <> short 'o'
+                    <> metavar "FILE"
+                    <> value "out.txt"
+                    <> help "Write output to FILE" )
 
 optsWithHelp :: ParserInfo Commands
 optsWithHelp =
@@ -83,20 +90,19 @@ run :: Commands -> IO ()
 run RunServer = putStrLn "runserver"
 run (RunProblem prob) = executeCommand $ go prob
   where     
-    go (Options c@Hamm e) = (c, e, return . fromEither . mapRight show . ProbHamm.findSubsAndPrintFromInput)
-    go (Options c@Revc e) = (c, e, return . ProbRevc.revc)
-    go (Options c@Rna e) = (c, e, return . fromEither . ProbRna.prob)
-    go (Options c@Revc2 e) = (c, e, return . fromEither . mapRight show . ProbRevc.prob)
-    go (Options c@Prot e) = (c, e, return . fromEither  . ProbProt.prob)
-    go (Options c@Tran e) = (c, e, return . fromEither  . ProbTran.prob)
-    executeCommand (c, dataSetOption, f) = do
+    go (Options c@Hamm e o) = (c, e,o, return . fromEither . mapRight show . ProbHamm.findSubsAndPrintFromInput)
+    go (Options c@Revc e o) = (c, e,o, return . ProbRevc.revc)
+    go (Options c@Rna e o ) = (c, e,o, return . fromEither . ProbRna.prob)
+    go (Options c@Revc2 e o) = (c, e,o, return . fromEither . mapRight show . ProbRevc.prob)
+    go (Options c@Prot e o) = (c, e,o, return . fromEither  . ProbProt.prob)
+    go (Options c@Tran e o) = (c, e,o, return . fromEither  . ProbTran.prob)
+    executeCommand (c, dataSetOption,outputFilename, f) = do
       baseDir <- getCurrentDirectory <&>  (</> "Data")
       let s = filter isLetter $ getCommandName c
           e' = case dataSetOption of
             ExampleDataset -> "_example"
             RealDataset -> ""
           inputFilename = s <> e' <> ".txt"
-          outputFilename = s <> e' <> ".out"
       putStrLn $ "Dataset option: " <> show dataSetOption
       putStrLn $ "Evaluating " <> s <> " -> " <> inputFilename
       readFile (baseDir </> inputFilename)
