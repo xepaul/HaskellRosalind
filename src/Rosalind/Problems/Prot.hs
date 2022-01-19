49021b526123bb81a4c82cef03dad984a41fbc7f
xepaul <xepaul@users.noreply.github.com>
2022-01-19 11:05:10 +0000

    tidy prob prot

--- src/Rosalind/Problems/Prot.hs
+++ src/Rosalind/Problems/Prot.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Rosalind.Problems.Prot
   (
      convertToProtein
@@ -12,22 +13,22 @@ import Rosalind.Codon2ProteinConv ( rnaCodon2ProteinWithStop )
 import Rosalind.RnaBase ( RnaBase, parseRnaBases )
 import Rosalind.ProteinWithStop
     ( proteins2String, ProteinWithStop(Stop) )
+import Data.Either.Combinators (maybeToRight)
+import Control.Monad.Except (MonadError (throwError))
 
 prob :: String -> Either String String
 prob s =  proteins2String <$> convertToProtein s
 
 convertToProtein :: [Char] -> Either [Char] [ProteinWithStop ]
-convertToProtein x =
-        do
-          convertRnaBasesToProtein =<< parseRnaBases  x
+convertToProtein x = convertRnaBasesToProtein =<< parseRnaBases  x
           
 convertRnaBasesToProtein :: [RnaBase] -> Either [Char] [ProteinWithStop ]
 convertRnaBasesToProtein r = do
-        codons <- traverse ensure3RnaBases $ chunksOf 3 r
+        codons <- maybeToRight "partial codons" $ traverse ensure3RnaBases $ chunksOf 3 r
         return $  takeWhile (/= Stop) $ map rnaCodon2ProteinWithStop codons
 
 
-  where ensure3RnaBases :: [a] -> Either String (a,a,a)
+  where ensure3RnaBases ::(MonadError () m) => [a] -> m (a,a,a)
         ensure3RnaBases = \case
-                          [a,b,c] -> Right (a,b,c)
-                          otherwise -> Left "partial codon"
+                          [a,b,c] -> return (a,b,c)
+                          _ -> throwError ()
