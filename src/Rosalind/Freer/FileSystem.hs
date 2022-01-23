42b44a66ca09c06a13a2ac6584250edc478fc306
Paul Strong <xepaul@hotmail.co.uk>
2022-01-23 15:09:28 +0000

    remove getCurrentDirectory from FileSystem

--- src/Rosalind/Freer/FileSystem.hs
+++ src/Rosalind/Freer/FileSystem.hs
@@ -6,16 +6,13 @@
 {-# LANGUAGE DataKinds #-}
 module Rosalind.Freer.FileSystem where
 import Control.Monad.Freer (Member, Eff, send, LastMember, interpretM, type (~>), reinterpret)
-import System.Directory (getCurrentDirectory)
---import Control.Monad.State (State, evalState, MonadState (get), modify)
+import Control.Monad.Freer.State (State, evalState, get, modify)
 import Data.List (deleteBy)
 import Data.Function (on)
-import Control.Monad.Freer.State (State, evalState, get, modify)
 
 data FileSystem r where
   ReadFile :: FilePath -> FileSystem String
   WriteFile :: FilePath -> String -> FileSystem ()
-  GetCurrentDirectory :: FileSystem FilePath
 
 readFile' :: Member FileSystem effs => FilePath -> Eff effs String
 readFile' path = send (ReadFile path)
@@ -23,15 +20,11 @@ readFile' path = send (ReadFile path)
 writeFile' :: Member FileSystem effs => FilePath -> String -> Eff effs ()
 writeFile' path contents = send (WriteFile path contents)
 
-getCurrentDirectory' :: Member FileSystem effs =>  Eff effs FilePath
-getCurrentDirectory' = send GetCurrentDirectory
-
 runFileSystemM :: forall effs a. LastMember IO effs
             => Eff (FileSystem ': effs) a -> Eff effs a
 runFileSystemM = interpretM $ \case
   ReadFile msg -> readFile msg
   WriteFile filePath content -> writeFile filePath content
-  GetCurrentDirectory -> getCurrentDirectory
 
 runInMemoryFileSystem :: [(FilePath, String)] -> Eff (FileSystem ': effs) ~> Eff effs
 runInMemoryFileSystem initVfs = evalState initVfs . fsToState where
@@ -41,5 +34,4 @@ runInMemoryFileSystem initVfs = evalState initVfs . fsToState where
                                 Just contents -> pure contents
                                 Nothing -> error ("readFile: no such file " ++ path)
                               WriteFile path contents -> modify $ \vfs ->
-                                (path, contents) : deleteBy ((==) `on` fst) (path, contents) vfs
-                              GetCurrentDirectory -> return ""
+                                (path, contents) : deleteBy ((==) `on` fst) (path, contents) vfs                              
