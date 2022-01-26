20363ac27c453810a5a9ceedc026c4823f766224
xepaul <xepaul@users.noreply.github.com>
2022-01-26 09:51:05 +0000

    add lexf prob

---
+++ src/Rosalind/Problems/Lexf.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Rosalind.Problems.Lexf where
+
+import Data.Either (fromRight)
+import Data.List qualified as List
+
+prob :: String -> Either String String
+prob s = 
+  case lines s of
+    [a, b] ->
+      let alphabet = words a
+          n = read @Int b
+          raw'' = enumerateKmers n alphabet
+       in Right $ unlines raw''
+    _ -> Left "bad"
+
+
+enumerateKmers :: Int -> [String] -> [[Char]]
+enumerateKmers n alphabet = let alphabet' = List.sort alphabet
+  in map (concatMap (alphabet' List.!!) . reverse) $ reverse $ enumerateKmersNum n (length alphabet)
+  where
+    enumerateKmersNum :: Int -> Int -> [[Int]]
+    enumerateKmersNum w b =
+      let initial = replicate w (0 :: Int)
+      in fst $
+            foldl
+              ( \(s, v) _ ->
+                  let v' = inc1 0 v
+                      s' = v : s
+                  in (s', v')
+              )
+              ([], initial)
+              [0 .. b ^ w -1]
+      where
+        inc1 :: Int -> [Int] -> [Int]
+        inc1 carry (x : xs) =
+          let v = x + 1 + carry
+          in if v >= b
+                then (v - b) : inc1 (v - b) xs
+                else v : xs
+        inc1 _ [] = []
---
+++ test/Spec/Rosalind/Problems/LexfHedgehogSpec.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Spec.Rosalind.Problems.LexfHedgehogSpec (test_tests) where
+
+
+import Data.List qualified as List
+import Data.Set qualified as Set
+import Data.Set (Set)
+import Hedgehog
+import Rosalind.Problems.Lexf qualified as Lexf
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.Hedgehog
+import Rosalind.RnaBase
+import Rosalind.ProteinWithStop
+import Spec.Rosalind.Common
+    ( testExampleDatasetMatchesExpected, ExampleNumber(Example1) )
+import qualified Hedgehog.Range as Range
+import qualified Hedgehog.Gen as Gen
+import qualified Test.Tasty.Hedgehog as H
+
+test_tests :: TestTree
+test_tests =
+  testGroup
+    "Unit tests Rosalind lexf (Hedgehog)"
+    [ testCase "lexf file expected " $ do
+      testExampleDatasetMatchesExpected Example1  "lexf"  Lexf.prob
+      , H.testProperty "check sample result" prop2
+    ]
+
+genAlphabet :: Gen (Set String )
+genAlphabet = Gen.set (Range.linear 1 3 ) (Gen.element (map (: []) ['A'..'Z']))
+
+
+prop2 :: Hedgehog.Property
+prop2 = Hedgehog.property $ do
+  a <- forAll genAlphabet
+  n <- forAll $ Gen.element [(1::Int) ..3]
+  let r = Lexf.enumerateKmers n (Set.toList a)
+  Hedgehog.assert $ length (List.nub r) == length a^n
+
+
